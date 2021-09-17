6cab26423c917696ecc51e0b6322db734141c7b7
IT-NewgatePhan <newgate.p@itcgroup.io>
2021-09-17 10:42:31 +0700

    update-Note

--- nestjs-beginner/src/app.controller.ts
+++ nestjs-beginner/src/app.controller.ts
@@ -5,11 +5,11 @@ import { AppService } from './app.service';
 
 //this decorator Controller does not filter a path
 // => only your-domain.com/ will trigger the @Get() below
-@Controller()
+@Controller('path')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('path2')
   getHello(): string {
     return this.appService.getHello();
   }
